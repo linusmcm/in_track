317d2b74d1c71562253efabb15a7fef44bfe6d81
linusmcm <linus.mcmanamey@gmail.com>
2019-08-13 00:06:00 +1000

    new strategy modal completed

--- global.R
+++ global.R
@@ -5,14 +5,13 @@ library(dbplyr)
 library(bs4Dash)
 library(echarts4r)
 library(shinyWidgets)
-
 library(shinymaterial)
 # ------------------------------------------------------------- #
-#install.packages("golem")
-library(golem)
+#library(golem)
+#library(gt)
+#library(RJDBC)
 #install.packages("shinymaterial")
-library(gt)
-library(RJDBC)
+#install.packages("golem")
 #install.packages("RJDBC",dep=TRUE)
 #remotes::install_github("rstudio/gt")
 # ------------------------------------------------------------- #
@@ -24,7 +23,6 @@ buttonSize <<- "sm"
 # ------------------------------------------------------------- #
 databaseName <<- "g6Mj2lugZA"
 # ------------------------------------------------------------- #
-
 options(mysql = list(
     "host" = "remotemysql.com",
     "port" = 3306,
@@ -33,8 +31,6 @@ options(mysql = list(
 ))
 # ------------------------------------------------------------- #
 options(shiny.trace=TRUE)
-
-
 # ------------------------------------------------------------- #
 saveData <- function(db, data) 
 {
@@ -68,29 +64,54 @@ load_nav_bar_menu <- function()
     return(colleges)
 }
 # ------------------------------------------------------------- #
-#dbDisconnect(db)
-temp <- load_nav_bar_menu()
+write_strategy <- function(c_id, s_name, s_description)
+{
+    db <- dbConnect(MySQL()
+                    , dbname = databaseName
+                    , host = options()$mysql$host
+                    , port = options()$mysql$port
+                    , user = options()$mysql$user
+                    , password = options()$mysql$password)
+    
+    #s_name <- "test name"
+    #s_description <- "description blah blah"
+    t_df <- data.frame(strategy_id = round(runif(1, 1, 90000000),0)
+                       , strategy_name = s_name
+                       , strategy_description = s_description
+                       , college_id = c_id
+                       , active_strategy = "y")
+    
+    t_df <- t_df %>% mutate_if(is.factor, as.character)
+    table <- "strategy"
+    query <- sprintf("INSERT INTO %s (%s) VALUES ('%s')"
+                        , table
+                        , paste(names(t_df), collapse = ", ")
+                        , paste(t_df, collapse = "', '"))
+    
+    dbGetQuery(db, query)
+    dbDisconnect(db)
+}
+#temp <- load_nav_bar_menu()
 # ------------------------------------------------------------- #
 # ------------------------------------------------------------- #
 strategy_load <- function(failed = FALSE) 
 {
     modalDialog(
-          size = "l"
+          size = "m"
         , easyClose = T
-        , title = "Load Strategy",
-        #uiOutput("welcomeText"),
-        #uiOutput("overAll_sections"),
-        #uiOutput("sub_section"),
-        #uiOutput("speciality_select"),
-        #uiOutput("theCalender"),
-        
+        , fade = T
+        , title = uiOutput("collegeTitle")
+        , uiOutput("new_title")
+        , uiOutput("new_description")
+        #, uiOutput("theCalender")
+        , 
         if (failed)
             div(tags$b(" ", style = "color: red;")),
         
         footer = tagList(
            # h6(textOutput("versionText")),
             #modalButton("Cancel"),
-            actionButton("okR", "OK")
+            actionButton("strategy_ok", "OK")
         )
     )
 }
--- server.R
+++ server.R
@@ -1,68 +1,32 @@
 shinyServer(function(input, output) 
 {
+# ------------------------------------------------------------- #
+# INITIAL Loader ######
 # ------------------------------------------------------------- #    
-nav_bar_df <- reactive(load_nav_bar_menu())
-# ------------------------------------------------------------- #
-# NAV BAR Buttons ######
-# ------------------------------------------------------------- #
-output$firstButton <- renderUI(
-{
-    actionBttn(inputId = nav_bar_df()$college_short_name[1]
-               , label = nav_bar_df()$college_long_name [1]
-               , size = buttonSize
-               , icon = NULL
-               , style= buttonStyle
-               #, block = T 
-               )
-})
-# ------------------------------------------------------------- #
-output$secondButton <- renderUI(
-{
-        actionBttn(inputId = nav_bar_df()$college_short_name[2]
-                   , label = nav_bar_df()$college_long_name [2]
-                   , size = buttonSize
-                   , icon = NULL
-                   , style= buttonStyle
-                   #, block = T 
-                   )
-})
-# ------------------------------------------------------------- #
-output$thirdButton <- renderUI(
-{
-        actionBttn(inputId = nav_bar_df()$college_short_name[3]
-                   , label = nav_bar_df()$college_long_name [3]
-                   , size = buttonSize
-                   , icon = NULL
-                   , style= buttonStyle
-                   #, block = T 
-                   )
-})
-# ------------------------------------------------------------- #
-output$fourthButton <- renderUI(
-{
-        actionBttn(inputId = nav_bar_df()$college_short_name[4]
-                   , label = nav_bar_df()$college_long_name [4]
-                   , size = buttonSize
-                   , icon = NULL
-                   , style= buttonStyle
-                   #, block = T 
-                    )
-})
-
-
-# ------------------------------------------------------------- #
-# ------------------------------------------------------------- #
-# inital load page ######
 output$body <- renderUI({ fluidPage("Summary", summary_moduleUI("summary_interface")) })
 output$left_side_nav_buttons <- renderUI({   })
+# ------------------------------------------------------------- #
+# ------------------------------------------------------------- #    
+# REACTIVE VALUES ###################################
+# ------------------------------------------------------------- #    
+college_id <- reactiveVal(NULL)
+panel_Type <- reactiveVal(NULL)
+description_name <- reactiveVal(NULL)
+strategy_name <- reactiveVal(NULL)
+# ------------------------------------------------------------- #    
+nav_bar_df <- reactive(load_nav_bar_menu())
+
+# ------------------------------------------------------------- #
 # ------------------------------------------------------------- #
 # COSE ####
 # ------------------------------------------------------------- #
 observeEvent(input$COSE,
 {
     nav_bar_item <- nav_bar_df() %>% filter(college_short_name == "COSE")
+    college_id(nav_bar_item)
     output$sideTilte <- renderUI({ bs4SidebarUserPanel(img = "utas-logo-int.png", text = substring(nav_bar_item$college_long_name, 12)) })
     # output$body <- renderUI({ fluidPage("Main", main_module_UI("main_interface")) })
+    # --------------------------------------------------------- #
     output$left_side_nav_buttons <- renderUI(
         { 
         tagList(
@@ -73,6 +37,7 @@ observeEvent(input$COSE,
                    , style= buttonStyle
                    , block = T))
         })
+    # --------------------------------------------------------- #
     output$body <- renderUI(
         {
             #___BODY - Speciality_Overview ####
@@ -81,8 +46,8 @@ observeEvent(input$COSE,
                 , width = 12 
                 , title = nav_bar_item$college_long_name
                 #, headerBorder = T
-                # ------------------------------------------------------------------------ #git checkout 
-                # ------------------------------------------------------------------------ #
+                # --------------------------------------------- #
+                # --------------------------------------------- #
                 , bs4TabPanel(
                     tabName = paste0("Strategy - ", nav_bar_item$college_short_name)
                     , active = TRUE
@@ -90,7 +55,7 @@ observeEvent(input$COSE,
                     , "Summary"
                        #  , summary_moduleUI("summary_interface")
                 ), 
-                # ------------------------------------------------------------------------ #
+                # --------------------------------------------- #
                 bs4TabPanel(
                     tabName = paste0("Initiative - ", nav_bar_item$college_short_name)
                     , active = F
@@ -104,7 +69,7 @@ observeEvent(input$COSE,
                     , "DMS Item Codes"
                         # , dms_item_codes_moduleUI("dms_item_codes_interface")
                 ),
-                # ------------------------------------------------------------------------ #
+                # --------------------------------------------- #
                 bs4TabPanel(
                     tabName = paste0("Spare - ", nav_bar_item$college_short_name)
                     , active = F
@@ -112,8 +77,8 @@ observeEvent(input$COSE,
                     , "Provider Group Profile"
                         # , pgp_moduleUI("pgp_interface")
                 )
-                # ------------------------------------------------------------------------ #
-                # ------------------------------------------------------------------------ #
+                # --------------------------------------------- #
+                # --------------------------------------------- #
             ) # end tabBox
         }) # END renderUI
 })
@@ -123,6 +88,7 @@ observeEvent(input$COSE,
 observeEvent(input$COBE,
 {
     nav_bar_item <- nav_bar_df() %>% filter(college_short_name == "COBE")
+    college_id(nav_bar_item)
     output$sideTilte <- renderUI({ bs4SidebarUserPanel(img = "utas-logo-int.png", text = substring(nav_bar_item$college_long_name, 12)) })
 })
 # ------------------------------------------------------------- #
@@ -131,6 +97,7 @@ observeEvent(input$COBE,
 observeEvent(input$CALE,
 {
     nav_bar_item <- nav_bar_df() %>% filter(college_short_name == "CALE")
+    college_id(nav_bar_item)
     output$sideTilte <- renderUI({ bs4SidebarUserPanel(img = "utas-logo-int.png", text = substring(nav_bar_item$college_long_name, 12)) })
 })
 # ------------------------------------------------------------- #
@@ -139,30 +106,107 @@ observeEvent(input$CALE,
 observeEvent(input$COHM,
 {
     nav_bar_item <- nav_bar_df() %>% filter(college_short_name == "COHM")
+    college_id(nav_bar_item)
     output$sideTilte <- renderUI({ bs4SidebarUserPanel(img = "utas-logo-int.png", text = substring(nav_bar_item$college_long_name, 12)) })
 })
-# ------------------------------------------------------------------------ #
-observeEvent(input$add_stratergy,
-             {
-                 showModal(strategy_load())
-             })
-# ------------------------------------------------------------------------ #
-# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------- #
 # MODULES ###################################
-# ------------------------------------------------------------------------ #
-# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------- #
+# ------------------------------------------------------------- #
 # summary_module ####
-# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------- #
 callModule(summary_module
            , "summary_interface"
            , nav_bar_df = reactive(nav_bar_df()))
-# ------------------------------------------------------------------------ #
-# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------- #
+# main_module ####
+# ------------------------------------------------------------- #
 callModule(main_module
            , "main_interface"
            , nav_bar_df = reactive(nav_bar_df()))
-# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------- #
 # END MODULES ###################################
-# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------- #
+# ------------------------------------------------------------- #
+# NAV BAR Buttons ######
+# ------------------------------------------------------------- #
+output$firstButton <- renderUI(
+{
+        actionBttn(inputId = nav_bar_df()$college_short_name[1]
+                   , label = nav_bar_df()$college_long_name[1]
+                   , size = buttonSize
+                   , icon = NULL
+                   , style= buttonStyle)
+})
+# ------------------------------------------------------------- #
+output$secondButton <- renderUI(
+{
+        actionBttn(inputId = nav_bar_df()$college_short_name[2]
+                   , label = nav_bar_df()$college_long_name[2]
+                   , size = buttonSize
+                   , icon = NULL
+                   , style= buttonStyle)
+})
+# ------------------------------------------------------------- #
+output$thirdButton <- renderUI(
+{
+        actionBttn(inputId = nav_bar_df()$college_short_name[3]
+                   , label = nav_bar_df()$college_long_name[3]
+                   , size = buttonSize
+                   , icon = NULL
+                   , style= buttonStyle)
+})
+# ------------------------------------------------------------- #
+output$fourthButton <- renderUI(
+{
+        actionBttn(inputId = nav_bar_df()$college_short_name[4]
+                   , label = nav_bar_df()$college_long_name[4]
+                   , size = buttonSize
+                   , icon = NULL
+                   , style= buttonStyle)
+})
+# ------------------------------------------------------------- #
+# ------------------------------------------------------------- #
+# ------------------------------------------------------------- #
+# OBSERVE EVENTS ###################################
+# ------------------------------------------------------------- #
+observeEvent(input$add_strategy,
+{
+    showModal(strategy_load())
+    panel_Type("New  Strategy")
+    
+})
+# ------------------------------------------------------------- #
+observeEvent(input$strategy_ok,
+{
+    strategy_name(input$strategy_name)
+    description_name(input$description_name)
+    write_strategy(college_id()$college_short_name, req(strategy_name()), req(description_name()))
+})
+# ------------------------------------------------------------- #
+# ------------------------------------------------------------- #
+# UI ELEMENTS ###################################
+# ------------------------------------------------------------- #
+output$collegeTitle <- renderText(college_id()$college_long_name)
+output$panel_type <- renderText(panel_Type())
+output$new_title <- renderUI(
+{
+    textInput(inputId = "strategy_name"
+                , label = "New Strategy Name"
+                , placeholder = "Please add a Strategy Name"
+                , width = '450px')
+})
+# ------------------------------------------------------------- #
+output$new_description <- renderUI(
+    {
+        textAreaInput(inputId = "description_name"
+                  , label = "Strategy Description"
+                  , placeholder = "Please add a Strategy description"
+                  , width = '450px'
+                  , height = '450px')
+    })
+
+
+# ------------------------------------------------------------- #
 }) # END SERVER
-# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------- #
